64a0979534f2e361e513ae17b1d943ccb624119b
adevvv <adevvv@bk.ru>
2023-02-25 18:27:51 +0300

    Add fun editNotes

--- app/src/main/java/com/example/notes/ui/fragments/EditNotesFragment.kt
+++ app/src/main/java/com/example/notes/ui/fragments/EditNotesFragment.kt
@@ -1,22 +1,52 @@
 package com.example.notes.ui.fragments
 
 import android.os.Bundle
+import android.text.format.DateFormat
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.viewModels
+import androidx.navigation.Navigation
+import androidx.navigation.fragment.navArgs
 import com.example.notes.R
 import com.example.notes.databinding.FragmentEditNotesBinding
+import com.example.notes.model.Notes
+import com.example.notes.viewmodel.NotesViewModel
+import java.util.*
 
 class EditNotesFragment : Fragment() {
     lateinit var binding: FragmentEditNotesBinding
-
+    val oldNotes by navArgs<EditNotesFragmentArgs>()
+    val viewModel: NotesViewModel by viewModels()
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
     ): View? {
         binding = FragmentEditNotesBinding.inflate(layoutInflater, container, false)
+
+        binding.edtTitle.setText(oldNotes.data.title)
+        binding.edtNotes.setText(oldNotes.data.notes)
+
+        binding.flbSave.setOnClickListener {
+            editNotes(it)
+        }
         return binding.root
+    }
+
+    private fun editNotes(it: View?) {
+        val title = binding.edtTitle.text.toString()
+        val notes = binding.edtNotes.text.toString()
+        val d = Date()
+        val notesDate: CharSequence = DateFormat.format("MMMM d, yyyy", d.time)
+
+        val data = Notes(
+            oldNotes.data.id, title = title, notes = notes, date = notesDate.toString()
+        )
+        viewModel.updateNotes(data)
+
+        Navigation.findNavController(it!!)
+            .navigate((R.id.action_editNotesFragment_to_mainNotesFragment))
+
     }
 
 }
