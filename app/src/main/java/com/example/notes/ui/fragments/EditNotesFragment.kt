28957e9e489b6ae3c5089d0cc624ea4588e886c2
adevvv <adevvv@bk.ru>
2023-02-25 14:25:18 +0300

    Add Simple UI Design

---
+++ app/src/main/java/com/example/notes/ui/fragments/EditNotesFragment.kt
@@ -0,0 +1,22 @@
+package com.example.notes.ui.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.notes.R
+import com.example.notes.databinding.FragmentEditNotesBinding
+
+class EditNotesFragment : Fragment() {
+    lateinit var binding: FragmentEditNotesBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentEditNotesBinding.inflate(layoutInflater, container, false)
+        return binding.root
+    }
+
+}
---
+++ app/src/main/java/com/example/notes/ui/fragments/MainNotesFragment.kt
@@ -0,0 +1,29 @@
+package com.example.notes.ui.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.Navigation
+import com.example.notes.R
+import com.example.notes.databinding.FragmentMainNotesBinding
+
+class MainNotesFragment : Fragment() {
+    lateinit var binding: FragmentMainNotesBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentMainNotesBinding.inflate(layoutInflater, container, false)
+
+        binding.flbNewNotes.setOnClickListener {
+            Navigation.findNavController(it).navigate((R.id.action_mainNotesFragment_to_newNotesFragment))
+        }
+
+        return binding.root
+    }
+
+
+}
